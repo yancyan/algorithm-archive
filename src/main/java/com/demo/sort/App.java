9da7ec72d94386f2016b859b86000859db4a12a2
zhuyx <zhuyx@startimes.com.cn>
2020-09-16 14:54:26 +0800

    基础排序: 归并排序实现...

--- src/main/java/com/demo/sort/App.java
+++ src/main/java/com/demo/sort/App.java
@@ -7,7 +7,7 @@ package com.demo.sort;
  * *** 插入排序 / Insertion Sort
  *
  * 归并排序 / Merge Sort
- * 快速排序 / Quick Sort
+ * *** 快速排序 / Quick Sort
  * 拓扑排序 / Topological Sort
  *
  * 堆排序 Heap Sort
@@ -16,18 +16,18 @@ package com.demo.sort;
  * 拓展：
  * *** 希尔排序（Shell Sort）: ex(插入排序)
  * *** 选择排序（Selection Sort） ：ex(冒泡排序)
- * 快速排序（Quick Sort）
  * 计数排序（Counting Sort）
  * 基数排序（Radix Sort）
  */
 public class App {
 
     public static void main(String[] args) {
-        int[] ar = new int[]{1, 3, 2, 9, 23,4, 7};
+        int[] ar = new int[]{1, 3, 2, 9, 23,4, 7, 4};
         // MergeSort.sort(ar);
         // SelectionSort.sort(ar);
         // ShellSort.sort(ar);
-        QuickSort.sort(ar, 0, ar.length - 1);
+        // QuickSort.sort(ar, 0, ar.length - 1);
+        MergeSort.sort(ar);
         for (int i : ar) {
             System.out.print(i + ", ");
         }
